62f19be0243e9d62d7aad8bf6b2e396d029a8c2d
w68141 <yr.hvrn15@gmail.com>
2022-11-10 12:13:22 +0100

    jeden z poniszych komunikatów

--- 2.py
+++ 2.py
@@ -6,6 +6,15 @@ for i in range(n):
     x = random.randint(1, 10)
     zestaw_1.append(x)
 print(zestaw_1)
+
 n = int(input("Podaj rozmiar lista: "))
 zestaw_2 = [random.randint(5, 15) for i in range(n)]
 print(zestaw_2)
+
+liczba = int(input("Podaj liczbę: "))
+if liczba  in  zestaw_1:
+    print("Liczna z zestawu pierwszego.")
+elif liczba in zestaw_2:
+    print("Liczna z zestawu drugiego.")
+else:
+    print("Liczba nie występuje w żadnym z zestawów!")
